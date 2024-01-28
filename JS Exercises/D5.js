7f957a1811ab04c78ab41af27fe953c3ed87c611
Alice Alfaioli <alicealice5693@yahoo.it>
2024-01-28 20:48:56 +0100

    Second Commit

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -23,24 +23,25 @@ console.log(pets);
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
-
-pets.sort();
-
+console.log("PRIMA", pets);
+const petsCloned = pets.slice();
+petsCloned.sort();
 console.log(pets);
+console.log(petsCloned);
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 
-pets.reverse();
-
-pets.forEach((pet) => console.log(pet));
+console.log(petsCloned.reverse());
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+const firstElem = pets.shift();
+pets.push(firstElem);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -66,29 +67,98 @@ const cars = [
   },
 ];
 
+for (let i = 0; i < cars.lenght; i++) {
+  const carObj = cars[i];
+  carObj.licensePlate = "AB00" + i + "YZ";
+}
+
+console.log(cars);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+
+const newCar = {
+  brand: "volkswagen",
+  model: "Passat",
+  color: "silver",
+  trims: ["sport", "s-line"],
+  licensePlate: "AB003YZ",
+};
+
+cars.push(newCar);
+
+for (let i = 0; i < cars.lengh; i++) {
+  const carObj = cars[i];
+  console.log(carObj.trims);
+  if (carObj.trims) {
+    carObj.trims.pop();
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = [];
 
+for (let i = 0; i < cars.lengh; i++) {
+  const carObj = cars[i];
+  justTrims.push(carObj.trims[0]);
+}
+
+console.log(justTrims);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 
+for (let i = 0; i < cars.lengh; i++) {
+  const carObj = cars[i];
+
+  if (carObj.color.charAt(0) === "b") {
+    console.log("FIzz");
+  } else {
+    conaole.log("Buzz");
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105];
 
+let numbers = 0;
+
+let value = numericArray[numbers];
+while (value !== 32) {
+  console.log(value);
+  numbers++;
+  value = numericArray[numbers];
+}
+console.log(value);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ["g", "n", "u", "z", "d"];
+
+const positionArray = [];
+
+for (let character of charactersArray) {
+  if (alphabet[character]) {
+    positionArray.push(alphabet[character]);
+  }
+}
+
+console.log(positionArray);
